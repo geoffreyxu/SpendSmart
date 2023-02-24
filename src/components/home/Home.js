fd0227033eccc601ca88929fa0faa33bde7db5eb
Geoffrey  Xu <geoffreyxu@Geoffreys-Laptop.local>
2023-02-24 15:59:03 -0800

    Added Home.css component and budget feature

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Home.css'
 import { useAuth } from '../../context/UserAuthContext'
 import { Link, useNavigate } from "react-router-dom"
 import Header from '../header/Header'
@@ -8,10 +9,32 @@ const Home = () => {
   const { logout } = useAuth()
   const navigate = useNavigate()
 
+  const Balance = () => {
+    return (
+      <section classname="balance">
+        <div classname="balance-box">
+          <h2>Balance: $1,000</h2>
+        </div>
+      </section>
+    );
+  }
+  const Icons = () => {
+    return (
+      <section className="icons">
+        <div className="icon">
+          <i className="fas fa-shopping-cart"></i>
+          <p>Shop</p>
+        </div>
+      </section>
+    );
+  };
+
   return (
     <>
      <Header></Header>
       <h1>Welcome to the home page</h1>
+      <Balance />
+      <Icons />
     </>
   )
 }
