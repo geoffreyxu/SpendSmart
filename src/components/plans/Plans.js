bd2fb1b55f2b6ad88be21d0e031b8f3c77e2f0f0
Chris Head <chrishead86@wifi-131-179-51-159.host.ucla.edu>
2023-03-04 15:24:12 -0800

    Added imports and basic lines to start working on Calendar.

--- src/components/plans/Plans.js
+++ src/components/plans/Plans.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Header from '../header/Header'
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+const localizer = momentLocalizer(moment);
 
 function Plans() {
     
