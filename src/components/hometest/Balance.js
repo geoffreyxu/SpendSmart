3e853c86d17ac5b362bbbc3faef968f7194c7885
Geoffrey  Xu <geoffreyxu@Geoffreys-Laptop.local>
2023-03-16 10:22:59 -0700

    Implemented search bar

--- src/components/hometest/Balance.js
+++ src/components/hometest/Balance.js
@@ -53,9 +53,9 @@ function Balance() {
 return (
     <div className="balance-home">
         <div className="balance-header">
-      <h2>Your Balance</h2>
-      </div>
-      <h3>${balance}</h3>
+          <h2>Your Balance</h2>
+        </div>
+        <h3>${balance}</h3>
       <div className="income-expense-home">
         <div className="plus-home">
           <h3>Income</h3>
